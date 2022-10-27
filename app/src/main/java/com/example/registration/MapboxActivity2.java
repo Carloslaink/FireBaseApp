0c5ee288bb3c7ad2a652db9f48282d51134e4d79
Carloslaink <Anthonycarlosr@gmail.com>
2022-10-26 23:11:43 -0300

    implementando mapbox

---
+++ app/src/main/java/com/example/registration/MapboxActivity2.java
@@ -0,0 +1,36 @@
+package com.example.registration;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+public class MapboxActivity2 extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        setContentView(R.layout.activity_mapbox2);
+
+        mapView = (MapView) findViewById(R.id.mapbox);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {}
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+}
